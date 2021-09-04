953858b1fe0ff5374ac1f87a80860617c10be7a1
Angello Gómez <r9angello@hotmail.com>
2021-09-03 21:13:30 -0500

    [agregarjs]

--- src/app/components/nuevo/nuevo.component.ts
+++ src/app/components/nuevo/nuevo.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+declare var funcion1: any;
 @Component({
   selector: 'app-nuevo',
   templateUrl: './nuevo.component.html',
   styleUrls: ['./nuevo.component.css']
 })
 export class NuevoComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClick1(){
+    alert('iniciada sesión');
+  }
 }
